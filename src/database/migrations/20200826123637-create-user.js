e59504a794133a3ad5633f62962b550c05f8a03a
Chibuike <aniakuchibuike@gmail.com>
2020-08-26 14:30:21 +0100

    setting up the env

---
+++ src/database/migrations/20200826123637-create-user.js
@@ -0,0 +1,27 @@
+export default {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => queryInterface.dropTable('Users')
+};
